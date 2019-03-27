ebd694fec3f0e2987b3b26603fa1c422b3b9fce9
AlexKopec <kopecalex@gmail.com>
2019-03-27 17:37:35 -0500

    mobile navbar now working again

--- js/resume.js
+++ js/resume.js
@@ -15,3 +15,7 @@ $( '#navbarSupportedContent .navbar-nav a' ).on( 'click', function () {
 	$( '#navbarSupportedContent .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 	$( this ).parent( 'li' ).addClass( 'active' );
 });
+
+$('.js-scroll-trigger').click(function() {
+  $('.navbar-collapse').collapse('hide');
+});
