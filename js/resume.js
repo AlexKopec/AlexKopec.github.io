c6422405db08cab7df40ed3e7c00dd622f90b64f
AlexKopec <kopecalex@gmail.com>
2019-03-27 17:12:37 -0500

    MVP finished

---
+++ js/resume.js
@@ -0,0 +1,17 @@
+window.scroll({
+  top: 2500,
+  left: 0,
+  behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position
+window.scrollBy({
+  top: 100, // could be negative value
+  left: 0,
+  behavior: 'smooth'
+});
+
+$( '#navbarSupportedContent .navbar-nav a' ).on( 'click', function () {
+	$( '#navbarSupportedContent .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+	$( this ).parent( 'li' ).addClass( 'active' );
+});
